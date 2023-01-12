0e73a2460ef3e8afa85ea8b849b811e5b28c6bdc
FabrizioMisseri <misserifabrizio@gmail.com>
2023-01-12 10:06:28 +0100

    refactoring

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -2,11 +2,15 @@
 
 @section('content')
     <div class="container">
-        <h1 class="text-center mt-3">{{ $project->title }}</h1>
-        <div class="d-flex justify-content-between mt-3">
-            <h5>{{ $project->slug }}</h5>
-            <p>{{ $project->description }}</p>
+        <div class="row mt-4">
+            <div class="col-8 offset-2">
+                <h1 class="mt-3">{{ $project->title }}</h1>
+                <div class="mt-3">
+                    <h5>{{ $project->slug }}</h5>
+                    <p>{{ $project->description }}</p>
+                </div>
+                <p class="mt-3">{{ $project->content }}</p>
+            </div>
         </div>
-        <p class="mt-3">{{ $project->content }}</p>
     </div>
 @endsection
